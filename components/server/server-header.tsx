ee5470845c4667e367641f9877cf916b8efd13d6
jakukow <jakubkowolik00@gmail.com>
2024-09-22 22:29:33 +0200

    Invite implemented

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -11,12 +11,14 @@ import {
 } from "../ui/dropdown-menu";
 import {
   ChevronDown,
+  LogOut,
   PlusCircle,
   Settings,
   Trash,
   User,
   UserPlus,
 } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles;
@@ -24,6 +26,7 @@ interface ServerHeaderProps {
 }
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = useModal();
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
@@ -36,7 +39,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
         {isModerator && (
-          <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm  cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("invite", { server })}
+            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm  cursor-pointer"
+          >
             Invite People <UserPlus className="h-4 w-4 ml-auto" />{" "}
           </DropdownMenuItem>
         )}
@@ -59,6 +65,11 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         {isAdmin && (
           <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm  cursor-pointer">
             Delete Server <Trash className="h-4 w-4 ml-auto" />{" "}
+          </DropdownMenuItem>
+        )}
+        {!isAdmin && (
+          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm  cursor-pointer">
+            Leave Server <LogOut className="h-4 w-4 ml-auto" />{" "}
           </DropdownMenuItem>
         )}
       </DropdownMenuContent>
